ab22b6f5a4b3a83f355dc6c8acd96752c6195d40
mayconwallas <maycon.wallas.balbino@gmail.com>
2023-10-25 10:51:55 -0300

    estudos de entrada e de tipos de dados

---
+++ adivinhacao.py
@@ -0,0 +1,13 @@
+print("**********************************")
+print("Bem-vindo ao jogo de Adivinhação!!")
+print("**********************************")
+
+numero_secreto = 42
+chute = int(input("Digite um numero: "))
+
+print("Você digitou ", chute)
+if (numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
+ 
