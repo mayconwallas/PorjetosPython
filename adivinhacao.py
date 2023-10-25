3ddc108f0aced703f3f78ad26cdfd4653048492d
mayconwallas <maycon.wallas.balbino@gmail.com>
2023-10-25 12:43:57 -0300

    progredindo nos estudos de python

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,10 +4,18 @@ print("**********************************")
 
 numero_secreto = 42
 chute = int(input("Digite um numero: "))
+acertou = numero_secreto == chute
+nunSecretoMaior = numero_secreto > chute
+nunSecretoMenor = numero_secreto < chute
 
 print("Você digitou ", chute)
-if (numero_secreto == chute):
+if (acertou):
     print("Você acertou!")
 else:
     print("Você errou!")
- 
+    if(nunSecretoMaior):
+        print("número informado é menor que o número secreto")
+    if(nunSecretoMenor):
+        print("número informado é maior que o número secreto")
+
+print("Fim do jogo.")
